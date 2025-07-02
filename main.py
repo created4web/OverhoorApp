e5b30cc10faf51b0ea49d76cee1f7feab01a577f
created4web <119972747+created4web@users.noreply.github.com>
2025-07-02 12:18:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,48 @@
+import gtts
+from playsound import playsound
+import os
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-import gtts
-import os
-from kivy.core.audio import SoundLoader
-
-class VraagLezer(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(orientation='vertical', **kwargs)
-        self.vragen = self.lees_vragen('vragen_antwoorden.txt')
-        self.index = 0
-
-        self.vraag_label = Label(text="Druk op 'Volgende' om te starten", font_size='20sp')
-        self.add_widget(self.vraag_label)
-
-        self.button = Button(text="Volgende vraag", size_hint=(1, 0.3), font_size='20sp')
-        self.button.bind(on_press=self.volgende_vraag)
-        self.add_widget(self.button)
-
-    def lees_vragen(self, bestandsnaam):
-        with open(bestandsnaam, 'r', encoding='utf-8') as file:
-            return [regel.strip() for regel in file.readlines() if regel.strip()]
-
-    def volgende_vraag(self, instance):
-        if self.index < len(self.vragen):
-            vraag = self.vragen[self.index]
-            self.vraag_label.text = vraag
-            self.speel_vraag_af(vraag)
-            self.index += 1
-        else:
-            self.vraag_label.text = "Alle vragen zijn gesteld."
-            self.button.disabled = True
-
-    def speel_vraag_af(self, tekst):
-        filename = "vraag.mp3"
-        tts = gtts.gTTS(tekst, lang='nl')
-        tts.save(filename)
-        sound = SoundLoader.load(filename)
-        if sound:
-            sound.play()
 
 class VraagApp(App):
     def build(self):
-        return VraagLezer()
+        self.vragen = self.lees_vragen_uit_bestand("vragen_antwoorden.txt")
+        self.index = 0
+
+        self.layout = BoxLayout(orientation='vertical', padding=20, spacing=20)
+        self.vraag_label = Label(text=self.vragen[self.index], font_size=20)
+        self.volgende_knop = Button(text="Volgende", size_hint=(1, 0.2))
+        self.volgende_knop.bind(on_press=self.volgende_vraag)
+
+        self.layout.add_widget(self.vraag_label)
+        self.layout.add_widget(self.volgende_knop)
+
+        self.voorlees_vraag(self.vragen[self.index], self.index)
+
+        return self.layout
+
+    def lees_vragen_uit_bestand(self, bestandsnaam):
+        with open(bestandsnaam, 'r', encoding='utf-8') as file:
+            vragen = file.readlines()
+        return [vraag.strip() for vraag in vragen if vraag.strip() != ""]
+
+    def voorlees_vraag(self, vraag, index):
+        bestand_naam = f"vraag_{index}.mp3"
+        tts = gtts.gTTS(vraag, lang='nl')
+        tts.save(bestand_naam)
+        playsound(bestand_naam)
+        os.remove(bestand_naam)
+
+    def volgende_vraag(self, instance):
+        self.index += 1
+        if self.index < len(self.vragen):
+            self.vraag_label.text = self.vragen[self.index]
+            self.voorlees_vraag(self.vragen[self.index], self.index)
+        else:
+            self.vraag_label.text = "Einde van de vragenlijst!"
+            self.volgende_knop.disabled = True
 
 if __name__ == '__main__':
     VraagApp().run()
